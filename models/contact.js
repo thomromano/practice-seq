c70e025fc21a54b49ada5bee14a77959fa1fb817
thomromano <thomromano@gmail.com>
2018-05-26 13:57:21 -0500

    remove email from contact.js

--- models/contact.js
+++ models/contact.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
     var Contact = sequelize.define("Contact", {
       contact_name: DataTypes.STRING,
       phome_number: DataTypes.STRING,
-      email: DataTypes.STRING
+      
     });
   
     Contact.associate = function(models) {
